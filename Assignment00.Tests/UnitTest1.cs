0f608d956a032af7f6c2170f0dec303edbf153bb
MarcusSimonsen <56913755+MarcusSimonsen@users.noreply.github.com>
2022-09-02 14:04:10 +0200

    Echo input (#1)

--- Assignment00.Tests/UnitTest1.cs
+++ Assignment00.Tests/UnitTest1.cs
@@ -1,12 +1,26 @@
 using Xunit;
+using FluentAssertions;
+using System;
+using System.IO;
 
 namespace Assignment00.Tests;
 
+using System.Reflection;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Echoes_each_word()
     {
+        //Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
 
+        //Act
+        var program = Assembly.Load(nameof(Assignment00));
+        program.EntryPoint?.Invoke(null, new[]{new String[]{"Hello there"}});
+
+        //Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        Assert.Equal("Hello there\nHello there\nHello there", output);
     }
 }
