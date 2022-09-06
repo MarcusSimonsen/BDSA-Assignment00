54febb28ec86242402ccd6ce7777ef3effa75527
Marcus Henrik Simonsen <mhsi@itu.dk>
2022-09-06 12:49:22 +0200

    Tested and implemented IsLeapYear

--- Assignment00/Program.cs
+++ Assignment00/Program.cs
@@ -3,6 +3,11 @@ Console.WriteLine("Hello, World!");
 
 public class LeapYear
 {
+    /**
+    A year is a leap year iff:
+    * it is divisible by 4 and,
+    * not exactly divisible by 100, unless exactly divisible by 400.
+    */
     public static bool IsLeapYear(int year)
     {
         return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
